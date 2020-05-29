9ec081762ee5fafd1ba7d1e37c158657e3a61000
SaiAutomation99 <saiprasanth.qa@gmail.com>
2020-05-29 15:26:09 +0530

    added some more functionality

--- FileConcepts/src/FilesCreation/FileChecking.java
+++ FileConcepts/src/FilesCreation/FileChecking.java
@@ -1,10 +1,24 @@
 package FilesCreation;
 
+import java.io.File;
+
 public class FileChecking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
+		
+		File file=new File("D:\\Study\\Materials_Exception_Java");
+		File[] filesnames=file.listFiles();
+		
+		System.out.println(filesnames.length);
+	for (int i = 0; i < filesnames.length; i++) {
+		
+		String filename=filesnames[i].getName();
+		System.out.println(filename);
+		
+	}
 	}
 
 }
